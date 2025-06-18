16c085b5c921fb7c73b442454a2c5dc0b77df15d
Ethan Lee <LeethalEthan1@Outlook.com>
2025-06-18 18:25:50 -0500

    Initial Commit

---
+++ content_scripts/inject.js
@@ -0,0 +1,137 @@
+"use strict";
+class Equalizer {
+    constructor() {
+        this.eqBands = [32, 64, 125, 250, 500, 1000, 2000, 4000, 8000, 16000];
+        this.eqBandNodes = [];
+    }
+    async init(audioContext) {
+        if (this.audioContext)
+            return;
+        this.audioContext = audioContext;
+        let previousNode = null;
+        this.eqBands.forEach(band => {
+            let node = this.createEQNode(band);
+            if (previousNode)
+                previousNode.connect(node);
+            previousNode = node;
+        });
+        this.eqBandNodes[0].type = 'lowshelf';
+        this.eqBandNodes[this.eqBands.length - 1].type = 'highshelf';
+    }
+    createEQNode(frequency, Q = 1.4) {
+        let band = this.audioContext.createBiquadFilter();
+        band.frequency.value = frequency;
+        band.type = 'peaking';
+        band.Q.value = Q;
+        this.eqBandNodes.push(band);
+        return band;
+    }
+    setFrequencyGain(frequencyIndex, gain) {
+        this.eqBandNodes[frequencyIndex].gain.value = gain;
+    }
+    get entry() {
+        return this.eqBandNodes[0];
+    }
+    connect(node) {
+        this.eqBandNodes[this.eqBandNodes.length - 1].connect(node);
+    }
+}
+class Convolver {
+    async init(audioContext) {
+        if (this.audioContext)
+            return;
+        this.audioContext = audioContext;
+        this.createNodeTree();
+        this.setupNodeRoutes();
+        await this.loadSample();
+        this.dryGainNode.gain.value = 0.5;
+        this.wetGainNode.gain.value = 0.5;
+    }
+    createNodeTree() {
+        this.entryNode = this.audioContext.createGain();
+        this.dryGainNode = this.audioContext.createGain();
+        this.wetGainNode = this.audioContext.createGain();
+        this.convolverNode = this.audioContext.createConvolver();
+        this.exitNode = this.audioContext.createGain();
+    }
+    setupNodeRoutes() {
+        this.entryNode.connect(this.dryGainNode);
+        this.entryNode.connect(this.convolverNode);
+        this.convolverNode.connect(this.wetGainNode);
+        this.dryGainNode.connect(this.exitNode);
+        this.wetGainNode.connect(this.exitNode);
+    }
+    async loadSample() {
+        this.convolverNode.buffer = await this.loadAudioBuffer('./content_scripts/impulse.wav');
+    }
+    async loadAudioBuffer(path) {
+        try {
+            const url = browser.runtime.getURL(path);
+            const res = await fetch(url);
+            const arrayBuffer = await res.arrayBuffer();
+            return await this.audioContext.decodeAudioData(arrayBuffer);
+        }
+        catch (err) {
+            throw new Error('Failed to create buffer');
+        }
+    }
+    get entry() {
+        return this.entryNode;
+    }
+    connect(node) {
+        this.exitNode.connect(node);
+    }
+    set mix(newMix) {
+        this.dryGainNode.gain.value = 1 - newMix;
+        this.wetGainNode.gain.value = newMix;
+    }
+}
+class AudioHandler {
+    constructor() {
+        this.audioContext = new AudioContext();
+        this.targets = new Set();
+        this.entryNode = null;
+        this.equalizer = new Equalizer();
+        this.convolver = new Convolver();
+    }
+    async init() {
+        await this.equalizer.init(this.audioContext);
+        await this.convolver.init(this.audioContext);
+        this.entryNode = this.audioContext.createGain();
+        this.entryNode.connect(this.convolver.entry);
+        this.convolver.connect(this.equalizer.entry);
+        this.equalizer.connect(this.audioContext.destination);
+        await this.handleUserSettings();
+    }
+    attach(mediaElement) {
+        if (this.targets.has(mediaElement))
+            return;
+        this.targets.add(mediaElement);
+        let sourceNode = this.audioContext.createMediaElementSource(mediaElement);
+        sourceNode.connect(this.entryNode);
+    }
+    async handleUserSettings() {
+        let result = await browser.storage.local.get(['convolver_mix', 'low_eq', 'mid_eq', 'high_eq']);
+        this.convolver.mix = typeof result['convolver_mix'] === 'number' ? result['convolver_mix'] : 0.5;
+        browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+            // console.log(`[LeeAudio] Message Received: `, {action:message.action, value:message.value});
+            if (message.action === "set-convolver_mix")
+                this.convolver.mix = message.value;
+            if (message.action.startsWith('set-band-')) {
+                let bandIndex = this.equalizer.eqBands.indexOf(parseInt(message.action.replace('set-band-', '')));
+                this.equalizer.setFrequencyGain(bandIndex, message.value);
+            }
+        });
+    }
+}
+(async () => {
+    let audioHandler = new AudioHandler();
+    await audioHandler.init();
+    window.addEventListener("play", (e) => { if (e.target instanceof HTMLMediaElement)
+        audioHandler.attach(e.target); }, true);
+    const originalAudioPlay = Audio.prototype.play;
+    Audio.prototype.play = function () {
+        audioHandler.attach(this);
+        return originalAudioPlay.apply(this, arguments);
+    };
+})();
---
+++ popup/popup.ts
@@ -0,0 +1,74 @@
+class UserRangeOption {
+
+    name:string;
+    storageName:string;
+
+    containerElem!:HTMLDivElement;
+    inputElem!:HTMLInputElement;
+    labelElem!:HTMLLabelElement;
+    min:number;
+    max:number;
+    step:number;
+    default:number;
+
+    constructor(opts:{name:string, min?:number, max?:number, step?:number, default?:number}) {
+
+        this.name = opts.name;
+        this.storageName = this.name.toLowerCase().split(' ').join('_');
+        this.min = opts.min ?? 0;
+        this.max = opts.max ?? 1;
+        this.step = opts.step ?? 0.01;
+        this.default = opts.default ?? 0.5;
+
+        this.createElement();
+        this.setupStorage();
+
+    }
+
+    async setupStorage() {
+
+        // Load any settings
+        let result = await browser.storage.local.get(this.storageName);
+        this.inputElem.value = (typeof result[this.storageName] === 'number' ? result[this.storageName] : this.default).toString();
+
+        // Add Listener
+        this.inputElem.addEventListener('input', () => {
+            const value = parseFloat(this.inputElem.value);
+            browser.storage.local.set({ [this.storageName]: value });
+            browser.tabs.query({}).then((tabs) => {
+                for (const tab of tabs)
+                    if (tab.id !== undefined) browser.tabs.sendMessage(tab.id, { action: `set-${this.storageName}`, value });
+            });
+        });
+
+    }
+
+    createElement() {
+        this.containerElem = document.createElement('div');
+        this.containerElem.classList.add('option-container');
+        this.labelElem = document.createElement('label');
+        this.labelElem.innerText = this.name;
+        this.labelElem.classList.add('option-label');
+        this.containerElem.appendChild(this.labelElem);
+        this.inputElem = document.createElement('input');
+        this.inputElem.type = 'range';
+        this.inputElem.min = this.min.toString();
+        this.inputElem.max = this.max.toString();
+        this.inputElem.step = this.step.toString();
+        this.inputElem.value = this.default.toString();
+        this.inputElem.classList.add('option-range');
+        this.containerElem.appendChild(this.inputElem);
+        document.body.appendChild(this.containerElem);
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', async() => {
+
+    new UserRangeOption({name:'Convolver Mix'});
+    const eqBands = [32, 64, 125, 250, 500, 1000, 2000, 4000, 8000, 16000];
+    eqBands.forEach(band => {
+        new UserRangeOption({name:`band-${band}`, min: -20, max: 20});
+    })
+
+})
